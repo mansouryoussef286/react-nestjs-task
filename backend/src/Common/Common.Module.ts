82a6178a31866dafe1bda5494d7edbf6a9aa993a
mansouryoussef286@gmail.com <mansouryoussef286@gmail.com>
2025-12-10 14:05:43 +0200

    added throttling to the api

--- backend/src/Common/Common.Module.ts
+++ backend/src/Common/Common.Module.ts
@@ -4,9 +4,10 @@ import { AsyncLocalStorage } from 'async_hooks';
 import { AppConfig } from '@App/Config/App.Config';
 import { UserHelper } from './Helpers/CurrentUser.Helper.service';
 import { PasswordService } from './Helpers/Crypto.Helper.service';
+import { ThrottlingModule } from './Throttling/Throttling.Module';
 
 @Module({
-  imports: [AsyncLocalStorage],
+  imports: [AsyncLocalStorage, ThrottlingModule],
   providers: [
     AppConfig,
     {
